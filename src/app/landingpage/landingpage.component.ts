316ee1b2de1e2262c36a2cd23814e079b6b8afba
dhivyarrk <dhivyabharathi.radhakrishnanr@gmail.com>
2024-11-30 18:14:28 +0200

    Add landing page

---
+++ src/app/landingpage/landingpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-landingpage',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.scss'
+})
+export class LandingpageComponent {
+}
