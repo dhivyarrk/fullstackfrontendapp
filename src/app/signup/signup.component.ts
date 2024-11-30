9fcdb128b1aca254fdad11e97eb4122ce8067619
dhivyarrk <dhivyabharathi.radhakrishnanr@gmail.com>
2024-11-30 20:07:49 +0200

    Add Signup page

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {User} from '../models/user.model';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent {
+  user: User = {
+    user_name: '',
+    password: '', // New password field
+    membership: 'regular', // Default value
+    contact_number: undefined,
+    email_id: '',
+    user_type: 'customer', // Default value
+  }
+    constructor(private authService: AuthService, private router: Router) {}
+    onSubmit() {
+      this.authService.signup(this.user).subscribe({
+        next: (response) => {
+          alert('Signup successful!');
+          console.log("signup");
+          console.log(response);
+          localStorage.setItem('token', response.user.token); // Save JWT token
+          localStorage.setItem('user_type', response.user.user_type); // Save JWT token
+          this.router.navigate(['/dashboard']); // Navigate to products page
+        },
+        error: (error) => alert('Error during signup: ' + error.message),
+      });
+    }
+}
