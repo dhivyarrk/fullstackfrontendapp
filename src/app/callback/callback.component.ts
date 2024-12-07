e673f176c1d1eab4696d3b980d786db8801d71e6
dhivyarrk <dhivyabharathi.radhakrishnanr@gmail.com>
2024-12-08 01:24:34 +0200

    add sso change

--- src/app/callback/callback.component.ts
+++ src/app/callback/callback.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+//import { Router } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {API_URL} from '../env';
 import { firstValueFrom } from 'rxjs';
 import { constants } from 'node:zlib';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-callback',
@@ -16,16 +17,33 @@ import { AuthService } from '../auth.service';
 })
 export class CallbackComponent implements OnInit {
 
-  constructor(private router: Router, private http: HttpClient, private authService: AuthService) {}
+  // constructor(private router: Router, private http: HttpClient, private authService: AuthService) {}
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     console.log("in init");
     // Call function to handle the callback and extract data from URL
     //this.handleCallback();
-    this.handleCallback(); //.catch(err => console.error('handleCallback error:', err));
+    //this.handleCallback(); //.catch(err => console.error('handleCallback error:', err));
+    this.route.queryParams.subscribe(params => {
+      const userName = params['user_name'];
+      const membership = params['membership'];
+      const userType = params['user_type'];
+      const token = params['token'];
+  
+      if (userName && token) {    
+        localStorage.setItem('user', userName);
+        localStorage.setItem('token', token);
+        localStorage.setItem('user_type', userType);
+        this.router.navigate(['/dashboard']);
+      } else {
+        console.warn('Missing user data in query params');
+      }
+    });
 
   }
 
+/*
   handleCallback() {
     this.authService.handleoauthcallback().subscribe({
         next: (response) => { // Define the expected shape of 'response'
@@ -46,5 +64,5 @@ export class CallbackComponent implements OnInit {
         },
         error: (error: any) => alert('Signin sso failed: ' + error.error.error),
       });
-  }
+  } */
 }
